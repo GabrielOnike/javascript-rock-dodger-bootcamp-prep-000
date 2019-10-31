e681f6570f489683a63c7a6f37dcb7ca5cdcde96
Gabriel Onike <onikegabriel@gmail.com>
2019-10-31 14:56:29 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -117,7 +117,7 @@ function createRock(x) {
 function endGame(z) {
   z.splice(gameInterval);
   z.splice(ROCKS);
-  z.splice(document.removeEventListener('keydown', moveDodger);
+  z.splice(document.removeEventListener(moveDodger()));
 }
 
 function moveDodger(e) {
