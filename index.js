7f470a152fa7280c5842f7e11940d52f7853e8b5
Gabriel Onike <onikegabriel@gmail.com>
2019-10-31 14:46:07 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -79,7 +79,7 @@ function createRock(x) {
      * If a rock collides with the DODGER,
      * we should call endGame()
      */
-     if (ROCK.style.top === DODGER.style.top) {
+     if (checkCollision(rock)) {
        return endGame();
      }
     /**
